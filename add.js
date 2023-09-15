dcf1c5cc23e3519cd6ce6f6185164f73dab29ef6
lostie807 <calvinng1993@gmail.com>
2023-09-15 14:57:25 +0100

    Challenge 4 Done

---
+++ add.js
@@ -0,0 +1,5 @@
+const add_num = (a,b) => {
+    return(a + b);
+}
+
+module.exports = add_num;
---
+++ multiply.js
@@ -0,0 +1,5 @@
+const multiply_num = (a,b) => {
+    return(a * b);
+}
+
+module.exports = multiply_num;
