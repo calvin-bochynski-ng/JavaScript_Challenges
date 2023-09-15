a2be47372f53661422bb7df32c6659f017cb20b2
lostie807 <calvinng1993@gmail.com>
2023-09-15 14:13:28 +0100

    files

---
+++ Challenge_3.js
@@ -0,0 +1,12 @@
+const a = 4;
+const b = 6;
+
+console.log(a+b);
+console.log(a*b);
+
+
+
+const greeting = 'Hello ';
+const name_1 = 'Makers';
+
+console.log (greeting + name_1);
---
+++ index.js
@@ -0,0 +1,3 @@
+const name = 'calvin';
+
+console.log('Hi Javascript!');
