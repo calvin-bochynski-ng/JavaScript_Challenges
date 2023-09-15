e8829d8b0312e9cf82ee34fd297a6af75c4b7f58
lostie807 <calvinng1993@gmail.com>
2023-09-15 14:21:34 +0100

    files

--- Challenge_3.js
+++ Challenge_3.js
@@ -1,12 +1,25 @@
+//## Exercise
+
+//1. In the `node` REPL, declare two variables — `a` with value 4 and `b` with value 6.
+//2. Using `console.log`, print the result of:
+//    * `a + b` (should print 10)
+//    * `a * b` (should print 24)
+
 const a = 4;
 const b = 6;
 
 console.log(a+b);
 console.log(a*b);
 
-
+//# Challenge
+//Can you find the problem in the following program? Make the fix, so the code prints "Hello Makers" when typed into the `node` REPL.
+//```javascript
+//const greeting = 'Hello ';
+//const name = 'JavaScripters';
+//console.log greeting + name;
+//```
 
 const greeting = 'Hello ';
-const name_1 = 'Makers';
+const name_1 = 'JavaScripters';
 
 console.log (greeting + name_1);
